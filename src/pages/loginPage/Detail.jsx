75b7504e550b57b297a86b075f0849776d494b5d
SeolHyeona01 <sha0209@dgu.ac.kr>
2023-08-05 13:21:54 +0900

    feat: 회원가입 조건 및 디자인 구현 완료

---
+++ src/pages/loginPage/Detail.jsx
@@ -0,0 +1,120 @@
+//Detail.jsx
+import { useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  Container,
+  WhiteBox,
+  LogoImage,
+  Text,
+  InputForm,
+  InputForm2,
+  JoinWrapper,
+  JoinTxt,
+  SigninBtn,
+  Profile,
+  TopWrapper,
+} from "./LoginStyle";
+
+const Detail = () => {
+  const navigate = useNavigate();
+  const [nickname, setNickname] = useState("");
+  const [city, setCity] = useState("");
+  const [country, setCountry] = useState("");
+  const [language, setLanguage] = useState("");
+
+  const cityHandleChange = (e) => {
+    setCity(e.target.value);
+    console.log(city);
+  };
+  const nicknameHandleChange = (e) => {
+    setNickname(e.target.value);
+    console.log(nickname);
+  };
+  const countryhandleChange = (e) => {
+    setCountry(e.target.value);
+    console.log(country);
+  };
+  const languagehandleChange = (e) => {
+    setLanguage(e.target.value);
+    console.log(language);
+  };
+
+  const inputCheck = () => {
+    if (
+      join.country.value === "none" ||
+      join.city.value === "" ||
+      join.language.value === "none" ||
+      join.nickname.value === ""
+    ) {
+      alert("필수 입력 항목을 전부 입력하세요!");
+      console.log("false");
+      return false;
+    } else {
+      console.log("true");
+      alert("회원가입 성공");
+      navigate("/");
+      return true;
+    }
+  };
+  return (
+    <Container>
+      <WhiteBox>
+        <TopWrapper>
+          <LogoImage
+            onClick={() => navigate("/")}
+            src="src/image/logo.png"
+            alt="logo"
+          />
+          <Profile>+</Profile>
+        </TopWrapper>
+        <Text>Input your information!</Text>
+        <form name="join" onSubmit={inputCheck}>
+          <JoinWrapper>
+            <InputForm
+              name="country"
+              value={country}
+              onChange={countryhandleChange}
+            >
+              <option value="none">Country</option>
+              <option value="rKorea">republic of korea</option>
+              <option value="rKorea">republic of korea</option>
+              <option value="rKorea">republic of korea</option>
+            </InputForm>
+            <InputForm2
+              type="text"
+              name="city"
+              value={city}
+              onChange={cityHandleChange}
+              placeholder="city"
+            />
+            <JoinTxt>Use only small english letter/No space</JoinTxt>
+            <InputForm
+              name="language"
+              value={language}
+              onChange={languagehandleChange}
+            >
+              <option value="none">Language</option>
+              <option value="rKorea">korea</option>
+              <option value="rKorea">republic of korea</option>
+              <option value="rKorea">republic of korea</option>
+            </InputForm>
+            <InputForm2
+              type="text"
+              name="nickname"
+              value={nickname}
+              onChange={nicknameHandleChange}
+              placeholder="nickname"
+            />
+            <JoinTxt>
+              Use only small english letter/Up to 17 letters
+              <br /> No space/ only (.)and(_) available
+            </JoinTxt>
+            <SigninBtn type="submit">sign up!</SigninBtn>
+          </JoinWrapper>
+        </form>
+      </WhiteBox>
+    </Container>
+  );
+};
+
+export default Detail;
