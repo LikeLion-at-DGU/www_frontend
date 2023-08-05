4df3f011af9eb657f47de4888672c05c29ba256e
mk1329 <mk1329@dgu.ac.kr>
2023-08-05 22:02:54 +0900

    Feat: 월드레코드 페이지 기본구성 작성

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <>
       <GlobalStyle />
       <Nav />
-      <Mainpage />
+      {/* <Mainpage /> */}
       {/* <Login /> */}
       <Outlet />
     </>
--- src/pages/recordPage/Record.jsx
+++ src/pages/recordPage/Record.jsx
@@ -1,10 +1,21 @@
 // Record.jsx
 
+import { RecordBox, RecordContainer } from "./RecordStyle";
+
 const Record = () => {
   return (
-    <>
-      <p>Record page</p>
-    </>
+    <RecordContainer>
+      <p>설명글</p>
+      <RecordBox FlexDirect="row">
+        <RecordBox FlexDirect="column">
+          <p>버튼 두개</p>
+          <p>친구 최근글</p>
+        </RecordBox>
+        <RecordBox>오늘의 기록 랜덤</RecordBox>
+      </RecordBox>
+      <RecordBox>친구 최근글 나열</RecordBox>
+      <RecordBox>secrets of local</RecordBox>
+    </RecordContainer>
   );
 };
 
