39cc1f20436f88551b1b45963109a2f1a37e5054
SeolHyeona01 <sha0209@dgu.ac.kr>
2023-08-11 14:21:10 +0900

    Feat: 동행자 라우트

--- src/pages/companionPage/CompanionCards.jsx
+++ src/pages/companionPage/CompanionCards.jsx
@@ -9,15 +9,18 @@ import { WwwBox } from '../mainPage/MainpageStyle'
 import Views from '../../components/index/Views'
 import Comments from '../../components/index/Comments'
 import Like from '../../components/index/Like'
+import { useNavigate } from 'react-router-dom'
 
 
 function CompanionCards() {
+  const navigate = useNavigate();
+
   return (
-    <CompanionCard>
+    <CompanionCard onClick={() => navigate("/companion/1")}>
       <CompanionText width="626px" height="124px" padding="10px 20px">
         <CompanionTitle>
           <p>Today Hanoi nightscape!!</p>
-          <p style={{color: "#848484", fontSize: "0.8rem"}}>23m ago</p>
+          <p style={{ color: "#848484", fontSize: "0.8rem" }}>23m ago</p>
         </CompanionTitle>
 
         <WwwBox>
@@ -50,7 +53,7 @@ function CompanionCards() {
         <Image src={CompanionImg} />
       </Img>
     </CompanionCard>
-  )
+  );
 }
 
 export default CompanionCards
