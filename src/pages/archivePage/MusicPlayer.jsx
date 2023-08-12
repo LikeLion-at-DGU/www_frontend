85e6ed107dcb51578e1ee5f755ae6f0f71eba90b
kickcha <chasm030517@gmail.com>
2023-08-12 22:39:11 +0900

    achive 2차

--- src/pages/archivePage/MusicPlayer.jsx
+++ src/pages/archivePage/MusicPlayer.jsx
@@ -1,8 +1,26 @@
-//MusicPlayer.jsx
+// MusicPlayer.jsx
+import {
+  MusicContainer,
+  CdImg,
+  Player,
+  Asap,
+  PlayerContainer,
+} from "./ArchiveStyle";
+import musicplayer from "../../image/musicplayer.png";
+import cdimage from "../../image/cdimage.png";
+import asap from "../../image/asap.png";
 
 const MusicPlayer = () => {
-  <>
-    <p>MusicPlayer</p>
-  </>;
+  return (
+    <MusicContainer>
+      <p>Now Playing ... </p>
+      <Asap src={asap} alt="playing" />
+      <PlayerContainer>
+        <Player src={musicplayer} alt="music player" />
+        <CdImg src={cdimage} alt="cd image" />
+      </PlayerContainer>
+    </MusicContainer>
+  );
 };
+
 export default MusicPlayer;
--- src/pages/archivePage/Myfile.jsx
+++ src/pages/archivePage/Myfile.jsx
@@ -1,22 +1,48 @@
-import styled from "styled-components";
-import { Image } from "./ArchiveStyle";
+import React, { useState } from "react";
+import {
+  StackedImages,
+  FileContainer,
+  ClickableImage,
+  ClickableImageContainer,
+  MyRecord,
+  DailyRecord,
+  DataCard,
+  Companions,
+} from "./ArchiveStyle";
 import blueMyRecord from "../../image/blueMyRecord.png";
-import blueDaily from "../../image/blueDaily.png";
+import blueDailyRecord from "../../image/blueDailyRecord.png";
 import blueDataCard from "../../image/blueDataCard.png";
 import blueCompanions from "../../image/blueCompanions.png";
-import skyMyRecord from "../../image/skyMyRecord.png";
-import skyDaily from "../../image/skyDaily.png";
-import skyDataCard from "../../image/skyDataCard.png";
-import skyCompanions from "../../image/skyCompanions.png";
+import whiteMyRecord from "../../image/whiteMyRecord.png";
+import whiteDailyRecord from "../../image/whiteDailyRecord.png";
+import whiteDataCard from "../../image/whiteDataCard.png";
+import whiteCompanions from "../../image/whiteCompanions.png";
 
 const MyFile = () => {
   return (
-    <>
-      <Image src={skyMyRecord} alt="file" />
-      <Image src={blueDaily} alt="file" />
-      <Image src={blueDataCard} alt="file" />
-      <Image src={blueCompanions} alt="file" />
-    </>
+    <FileContainer>
+      <StackedImages>
+        <ClickableImageContainer>
+          <ClickableImage src={blueMyRecord} alt="MyRecord" />
+          <MyRecord>My Record</MyRecord>
+        </ClickableImageContainer>
+
+        <ClickableImageContainer>
+          <ClickableImage src={blueDailyRecord} alt="Daily" />
+          <DailyRecord>Daily Record</DailyRecord>
+        </ClickableImageContainer>
+
+        <ClickableImageContainer>
+          <ClickableImage src={blueDataCard} alt="DataCard" />
+          <DataCard>Data Card</DataCard>
+        </ClickableImageContainer>
+
+        <ClickableImageContainer>
+          <ClickableImage src={blueCompanions} alt="Companions" />
+          <Companions>Companions</Companions>
+        </ClickableImageContainer>
+      </StackedImages>
+    </FileContainer>
   );
 };
 
