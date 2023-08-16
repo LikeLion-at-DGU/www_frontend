0ec1e37c2d56e868482706e7122881883a8c0335
mk1329 <mk1329@dgu.ac.kr>
2023-08-17 07:47:34 +0900

    중간 커밋

--- src/pages/searchPage/SearchBar.jsx
+++ src/pages/searchPage/SearchBar.jsx
@@ -6,7 +6,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
 
-const SearchBar = ({onClick}) => {
+const SearchBar = ({ onClick }) => {
     const navigate = useNavigate();
     const [searchType, setSearchType] = useState("word");
     const [searchValue, setSearchValue] = useState("");
@@ -20,6 +20,15 @@ const SearchBar = ({onClick}) => {
         navigate(`/search?q=${e.target.value}`);
     };
 
+    // url에 검색어추가
+    const handleSearchURL = () => {
+        if (searchValue) {
+            onClick(searchValue);
+            // handleReset();
+            navigate(`/records/?search=${searchValue}`)
+        }
+    }
+
     return (
         <SearchBox>
             <SelectType value={searchType} onChange={(e) => handleSearchType(e.target.value)}>
@@ -30,10 +39,11 @@ const SearchBar = ({onClick}) => {
                 type="text"
                 placeholder={searchType === "word" ? "type a words." : "#city_keyword  please use this form!"}
                 value={searchValue}
-                // onChange={(e) => setSearchTerm(e.target.value)}
-                onChange={handleChange}
+                onChange={(e) => setSearchValue(e.target.value)}
+                // onChange={handleChange}
             />
-            <SearchBtn onClick={() => onClick(searchValue, searchType)} >
+            {/* <SearchBtn onClick={() => onClick(searchValue, searchType)} > */}
+            <SearchBtn onClick={handleSearchURL} >
                 <FontAwesomeIcon icon={faSearch} />
             </SearchBtn>
         </SearchBox>
--- src/pages/searchPage/SearchResults.jsx
+++ src/pages/searchPage/SearchResults.jsx
@@ -10,21 +10,21 @@ const SearchResults = ({ record }) => {
     const navigate = useNavigate();
 
     return (
-        <SearchResult onClick={() => navigate("/record/1")}>
+        <SearchResult onClick={() => navigate(`/record/${record.id}`)}>
             <Img width="122px" height="122px">
-                {/* <SmallImage src={recordImg} /> */}
-                <Image src={record.photos[0]} />
+                <SmallImage src={recordImg} />
+                {/* <Image src={record.photos[0]} /> */}
             </Img>
             <Text padding="10px 17px">
                 <RecordWriter>
                     <img></img>
-                    {/* <p>giogio222</p> */}
-                    <p>{record.writer}</p>
+                    <p>giogio222</p>
+                    {/* <p>{record.writer}</p> */}
                     {/* writer의 국가/도시로 변경해야함 */}
                     <span>Iyaly/milano</span>
                 </RecordWriter>
-                {/* <SearchTitle>Unexpected luck!!</SearchTitle> */}
-                <SearchTitle>{record.title}</SearchTitle>
+                <SearchTitle>Unexpected luck!!</SearchTitle>
+                {/* <SearchTitle>{record.title}</SearchTitle> */}
                 <TagBox>
                     <HashTag>#milano_restaurant</HashTag>
                 </TagBox>
